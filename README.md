cd5dcccd7af508958599ae2dece675adad1921f2
Guoliang Cao <gcao99@gmail.com>
2016-03-24 15:56:10 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# JSON Presenter
+
+A JavaScript library to present any JSON data in a nice format
+
+## NOTES
+
+This project is bootstraped using [this template](https://github.com/rbartoli/react-boilerplate)
+
---
+++ webpack.config.babel.js
@@ -0,0 +1,67 @@
+import path from 'path';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import WebpackNotifierPlugin from 'webpack-notifier';
+
+let devConfig = {
+    context: path.join(__dirname, 'src'),
+    entry: [
+        './app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build/assets/js'),
+        publicPath: 'assets/js/',
+        filename: 'app.js'
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        contentBase: 'public',
+        historyApiFallback: false
+    },
+    module: {
+        preLoaders: [
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                loader: 'eslint'
+            } 
+        ],
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]']
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                loaders: ['style', 'css', 'sass']
+            },
+            {
+                test: /\.(jpg|png|ttf|eot|woff|woff2|svg)$/,
+                exclude: /node_modules/,
+                loader: 'url?limit=100000'
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            { 
+                from: '../public', 
+                to: '/build' 
+            }
+        ]),
+        new WebpackNotifierPlugin()
+    ]
+};
+
+if (process.env.NODE_ENV === 'production') {
+    devConfig.devtool = '';
+    devConfig.devServer = {};
+}
+
+export default devConfig;
