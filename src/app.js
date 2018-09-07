53859fbec26b7376f9c672937d54399abe02acd6
Guoliang Cao <gcao99@gmail.com>
2018-09-06 23:22:44 -0400

    Exclude SET_PATH from Redux Chrome extension

--- src/app.js
+++ src/app.js
@@ -57,7 +57,7 @@ var initialState = {
 const store = createStore(
     reducers,
     initialState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({actionsBlacklist: ['SET_PATH']})
 );
 
 function _render() {
