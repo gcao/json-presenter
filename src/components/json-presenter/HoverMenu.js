d67707b9f90aaab97f191491cfbfdb39f4403f73
Guoliang Cao <gcao99@gmail.com>
2019-02-06 20:28:19 -0500

    Hover menu

---
+++ src/components/json-presenter/HoverMenu.js
@@ -0,0 +1,13 @@
+class HoverMenu extends Component {
+    render() {
+        return (
+            <ul class="dropdown-menu">
+                <li>Edit</li>
+                <li>Sort</li>
+                <li>Delete</li>
+            </ul>
+        );
+    }
+}
+
+export default HoverMenu;
